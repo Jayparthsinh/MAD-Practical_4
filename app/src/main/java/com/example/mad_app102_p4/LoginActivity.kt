97ceab8e375eb10744e7085281d34f0767d39cb5
Jayparthsinh <150107887+Jayparthsinh@users.noreply.github.com>
2023-11-07 12:52:47 +0530

    final

---
+++ app/src/main/java/com/example/mad_app102_p4/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mad_app102_p4
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val btnsignup: Button = findViewById(R.id.btn_signup)
+        val i = Intent(this,RegistrationActivity::class.java)
+        btnsignup.setOnClickListener {
+            startActivity(i)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mad_app102_p4/RegistrationActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mad_app102_p4
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+        val btnlogin: Button = findViewById(R.id.btn_login)
+        val i = Intent(this,LoginActivity::class.java)
+        btnlogin.setOnClickListener {
+            startActivity(i)
+        }
+    }
+}
